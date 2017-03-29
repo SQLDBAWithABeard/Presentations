60d620215683bc5d6b8da99ddcdf79cf33b8a495
Rob Sewell <mrrobsewell@outlook.com>
2017-03-29 11:59:19 +0100

    Using the NUC servers

--- Green is Good Red is Bad - Turning Your Checklists into Pester Tests/02 - Reports from Pester.ps1
+++ Green is Good Red is Bad - Turning Your Checklists into Pester Tests/02 - Reports from Pester.ps1
@@ -1,19 +1,33 @@
 ﻿## Setting up reports from Pester Tests
-
+## Welcome to PowerShell :-)
+Return "This is a demo Beardy!"
 psedit Git:\Functions\Test-OLA.ps1
 
-Test-OLAInstance -Instance 'ROB-SURFACEBOOK','ROB-SURFACEBOOK\DAVE' -Share 'C:\MSSQL\Backup' -CheckForBackups
+$SQLServers = 'SQL2005Ser2003','SQL2012Ser08AG1','SQL2012Ser08AG2','SQL2014Ser12R2','SQL2016N1','SQL2016N2','SQL2016N3','SQLVnextN1','SQLvNextN2'
+
+$dbs = 0
+foreach ($S in $SQLServers)
+{
+     $srv = New-Object Microsoft.SqlServer.Management.Smo.Server $S
+     $dbs += $srv.Databases.Count
+}
+
+Write-Output " We Have $($SQLServers.Count) Servers and $dbs Databases"
+Write-Output "Let's Check ALL the Jobs, Schedules, results, Files in about 100 seconds!!"
+
+Test-OLAInstance -Instance $SQLServers -Share 'C:\MSSQL\Backup' -CheckForBackups -NoDatabaseRestoreCheck
+##   Test-OLAInstance -Instance 'SQLVnextN2' -Share 'C:\MSSQL\Backup' -CheckForBackups -NoDatabaseRestoreCheck
 
 psedit Git:\Functions\Test-OLAInstance.ps1
 
-Test-OLAInstance -Instance 'ROB-SURFACEBOOK','ROB-SURFACEBOOK\DAVE' -Share 'C:\MSSQL\Backup' -CheckForBackups -Report
+Write-Output " But if you want a report, you may have to wait 3 minutes!!"
+Test-OLAInstance -Instance $SQLServers  -Share 'C:\MSSQL\Backup' -CheckForBackups -NoDatabaseRestoreCheck -Report
 
 ## But you can also create a Different report like this
 
 $Path = 'Git:\Functions\Test-OLA.ps1'
 
-$Servers = 'ROB-SURFACEBOOK','ROB-SURFACEBOOK\DAVE' 
-foreach($Server in $Servers)
+foreach($Server in $SQLServers)
 {
 
     $Script = @{
@@ -21,6 +35,7 @@ foreach($Server in $Servers)
     Parameters = @{ Instance = $Server;
     CheckForBackups =  $true;
     CheckForDBFolders =  $true;
+    NoDatabaseRestoreCheck= $true;
     Share = 'C:\MSSQL\Backup';
     }
     }
