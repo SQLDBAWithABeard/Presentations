33aa56e76bd26f60b600d83031a2beba62d0d090
Rob <Rob@TheBeard.local>
2017-04-26 20:33:06 +0100

    Servernames change adn pester needs updating

--- PSConfEU2017 - Using the SQL Provider/Pester Test for Presentation.ps1
+++ PSConfEU2017 - Using the SQL Provider/Pester Test for Presentation.ps1
@@ -43,27 +43,30 @@ Describe "Testing NUC" {
 			    }
     }
 Context "THEBEARD_Domain" {
-            $NUCServers = 'BeardDC1','BeardDC2','LinuxvNextCTP14','SQL2005Ser2003','SQL2012Ser08AG3','SQL2012Ser08AG1','SQL2012Ser08AG2','SQL2014Ser12R2','SQL2016N1','SQL2016N2','SQL2016N3','SQLVnextN1','SQLvNextN2'
+            $NUCServers = (Get-VM -ComputerName beardnuc | Where-Object {$_.State -eq 'Running'}).Name
             foreach($VM in $NUCServers)
                 {
                                  It "$VM Should respond to ping" {
 				(Test-Connection -ComputerName $VM -Count 1 -Quiet -ErrorAction SilentlyContinue) | Should be $True
 				}
                 }
+            It "Domain Controllers should be running" {
+                ($NUCServers -match 'DC').Count | Should Be 2
+            }
     }
 
     Context "SQL State" {
         $SQLServers = (Get-VM -ComputerName beardnuc | Where-Object {$_.Name -like '*SQL*' -and $_.Name -ne 'SQL2008Ser2008' -and $_.State -eq 'Running'}).Name
         foreach($Server in $SQLServers)
         {
-          $DBEngine = Get-service -ComputerName $Server -Name MSSQLSERVER
+          $DBEngine = Get-service -ComputerName $Server -Name MSSQLSERVER -ErrorAction SilentlyContinue
            It "$Server  DBEngine should be running" {
                 $DBEngine.Status | Should Be 'Running'
             }
            It "$Server DBEngine Should be Auto Start" {
             $DBEngine.StartType | Should be 'Automatic'
            }
-              $Agent= Get-service -ComputerName $Server -Name SQLSERVERAGENT
+              $Agent= Get-service -ComputerName $Server -Name SQLSERVERAGENT  -ErrorAction SilentlyContinue
               It "$Server Agent should be running" {
                   $Agent.Status | Should Be 'Running'
            }
@@ -78,16 +81,16 @@ Context "THEBEARD_Domain" {
 Describe "Testing XPS" {
   Context "XPS" {
         It "DBEngine is running" {
-            (Get-Service mssqlserver).Status | Should Be Running
+            (Get-Service mssqlserver -ErrorAction SilentlyContinue).Status | Should Be Running
         }
         It "SQL Server Agent is running" {
-            (Get-Service sqlserveragent).Status | Should Be Running
+            (Get-Service sqlserveragent -ErrorAction SilentlyContinue).Status | Should Be Running
         }
         It "DAVE DBEngine is running" {
-            (Get-Service mssql*Dave).Status | Should Be Running
+            (Get-Service mssql*Dave -ErrorAction SilentlyContinue).Status | Should Be Running
         }
         It "DAVE Agent is running" {
-            (Get-Service sqlagent*dave).Status | Should Be Running
+            (Get-Service sqlagent*dave -ErrorAction SilentlyContinue).Status | Should Be Running
         }
     }
       
