e9d288eeaaeb50c584711a0b0fee6ef7c3fc49bc
Rob Sewell <mrrobsewell@outlook.com>
2017-10-13 14:58:01 +0100

    to make it skip

--- SQL Saturday Gothenburg - Green is Good Red is Bad/Demo Number 2.ps1
+++ SQL Saturday Gothenburg - Green is Good Red is Bad/Demo Number 2.ps1
@@ -1,5 +1,5 @@
     # cd Presentations:\
-    & 'Presentations:\PS Saturday Stuttgart - Green is Good Red is Bad\Test-SQLSDefaults.ps1'
+    & 'Presentations:\SQL Saturday Gothenburg - Green is Good Red is Bad\Test-SQLSDefaults.ps1'
 
 ## This is how it works - but I have no VMs here
 
@@ -36,4 +36,42 @@
       LogSPBlitzToTableFrequency = 2 ; # 2 means Monday 
       LogSPBlitzToTableStartTime  = '03:00:00'}
       
-     # Test-SQLDefault @Parms
+     Test-SQLDefault @Parms    # cd Presentations:\
+    & 'Presentations:\SQL Saturday Gothenburg - Green is Good Red is Bad\Test-SQLSDefaults.ps1'
+
+## This is how it works - but I have no VMs here
+
+    $Parms = @{
+      Servers = 'ROB-XPS' ,'ROB-XPS\DAVE','ROB-XPS\SQL2016';
+      SQLAdmins = 'THEBEARD\Rob','THEBEARD\SQLAdmins';
+      BackupDirectory = 'C:\MSSQL\Backup';
+      DataDirectory = 'C:\MSSQL\Data\';
+      LogDirectory = 'C:\MSSQL\Logs\';
+      MaxMemMb = '4096';
+      Collation = 'Latin1_General_CI_AS';
+      TempFiles = 3 ;
+      OlaSysFullFrequency = 'Daily';
+      OlaSysFullStartTime = '00:00:00';
+      OlaUserFullSchedule = 'Weekly';
+      OlaUserFullFrequency = 1 ;## 1 for Sunday
+      OlaUserFullStartTime = '00:00:00';
+      OlaUserDiffSchedule = 'Daily';
+      OlaUserDiffFrequency = 1; ## 126 for every day except Sunday
+      OlaUserDiffStartTime = '00:00:00';
+      OlaUserLogSubDayInterval = 4;
+      OlaUserLoginterval = 'Hour';
+      HasSPBlitz = $true;
+      HasSPBlitzCache = $True; 
+      HasSPBlitzIndex = $True;
+      HasSPAskBrent = $true;
+      HASSPBlitzTrace =  $true;
+      HasSPWhoisActive = $true;
+      LogWhoIsActiveToTable = $true;
+      LogSPBlitzToTable = $true;
+      LogSPBlitzToTableEnabled = $true;
+      LogSPBlitzToTableScheduled = $true;
+      LogSPBlitzToTableSchedule = 'Weekly'; 
+      LogSPBlitzToTableFrequency = 2 ; # 2 means Monday 
+      LogSPBlitzToTableStartTime  = '03:00:00'}
+      
+     Test-SQLDefault @Parms
