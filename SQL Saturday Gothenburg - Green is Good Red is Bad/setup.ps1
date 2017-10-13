41a2294cf947cb7e123b39f5700f7435cf36cacf
Rob Sewell <mrrobsewell@outlook.com>
2017-10-13 14:57:10 +0100

    import usual module not dev branch

--- SQL Saturday Gothenburg - Green is Good Red is Bad/setup.ps1
+++ SQL Saturday Gothenburg - Green is Good Red is Bad/setup.ps1
@@ -1,13 +1,12 @@
 #Requires -Version 5
 #Requires -module dbatools
 $VerbosePreference = 'Continue'
-Import-Module 'C:\Program Files\WindowsPowerShell\Modules\Pester\4.0.3\Pester.psd1'
 Import-Module 'C:\Windows\System32\WindowsPowerShell\v1.0\Modules\Hyper-V\2.0.0.0\Hyper-V.psd1' 
 Import-Module 'C:\Windows\System32\WindowsPowerShell\v1.0\Modules\DnsServer\DnsServer.psd1' 
 Import-Module 'C:\Windows\System32\WindowsPowerShell\v1.0\Modules\NetAdapter\NetAdapter.psd1' 
 Import-Module 'C:\Windows\System32\WindowsPowerShell\v1.0\Modules\NetTCPIP\NetTCPIP.psd1' 
 # Import-Module 'C:\Windows\System32\WindowsPowerShell\v1.0\Modules\DnsClient\DnsClient.psd1' 
-Import-Module 'C:\Program Files\WindowsPowerShell\Modules\dbatools\0.9.25\dbatools.psd1'
+Import-Module dbatools
 
 $SQLInstances = 'ROB-XPS','ROB-XPS\DAVE','ROB-XPS\SQL2016'
 
