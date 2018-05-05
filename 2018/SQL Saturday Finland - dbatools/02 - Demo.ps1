8cbaad356269a6ce210cfd65395472271d2dd5d1
enterpriseadmin@THEBEARD.LOCAL <enterpriseadmin@THEBEARD.LOCAL>
2018-05-05 10:46:14 +0000

    added builds

--- 2018/SQL Saturday Finland - dbatools/02 - Demo.ps1
+++ 2018/SQL Saturday Finland - dbatools/02 - Demo.ps1
@@ -1,28 +1,19 @@
+#region Setup Variables
 $containers = 'bearddockerhost,15789', 'bearddockerhost,15788', 'bearddockerhost,15787', 'bearddockerhost,15786'
+$SQLInstances = 'sql0','sql1'
 $filenames = (Get-ChildItem C:\SQLBackups\Keep).Name
 $cred = Import-Clixml $HOME\Documents\sa.cred
+#endregion
 
-$containers.ForEach{
-    $Container = $Psitem
-    $NameLevel = (Get-DbaSqlBuildReference -SqlInstance $Container -SqlCredential $cred).NameLevel
-    $NameLevel
-    switch ($NameLevel) {
-        2017 { 
-            Restore-DbaDatabase -SqlInstance $PSItem -SqlCredential $cred -Path C:\sqlbackups\ -useDestinationDefaultDirectories -WithReplace            
-        }
-        2016 {
-            $Files = $Filenames.Where{$PSitem -notlike '*2017*'}.ForEach{'C:\sqlbackups\' + $Psitem}
-            Restore-DbaDatabase -SqlInstance $Container -SqlCredential $cred -Path $Files -useDestinationDefaultDirectories -WithReplace            
-        }
-        2014 {
-            $Files = $Filenames.Where{$PSitem -notlike '*2017*' -and $Psitem -notlike '*2016*'}.ForEach{'C:\sqlbackups\' + $Psitem}
-            Restore-DbaDatabase -SqlInstance $Container -SqlCredential $cred -Path $Files -useDestinationDefaultDirectories -WithReplace            
-        }
-        2014 {
-            $Files = $Filenames.Where{$PSitem -like '*2012*'}.ForEach{'C:\sqlbackups\' + $Psitem}
-            Restore-DbaDatabase -SqlInstance $Container -SqlCredential $cred -Path $Files -useDestinationDefaultDirectories -WithReplace            
-        }
-        Default {}
-    }
+#region Look at Builds
+$builds = @()
+$SQLInstances.ForEach{
+    $builds += Get-DbaSqlBuildReference -SqlInstance $PSitem 
 }
 
+$containers.ForEach{
+    $Builds += Get-DbaSqlBuildReference -SqlInstance $PSitem -SqlCredential $cred
+}
+
+$Builds | Format-Table
+#endregion
