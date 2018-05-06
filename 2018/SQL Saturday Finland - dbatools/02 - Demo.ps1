f5956641a127f0f9b7fb1a7b749950c757c64fde
enterpriseadmin@THEBEARD.LOCAL <enterpriseadmin@THEBEARD.LOCAL>
2018-05-06 09:26:41 +0000

    add serach and help

--- 2018/SQL Saturday Finland - dbatools/02 - Demo.ps1
+++ 2018/SQL Saturday Finland - dbatools/02 - Demo.ps1
@@ -5,6 +5,39 @@ $filenames = (Get-ChildItem C:\SQLBackups\Keep).Name
 $cred = Import-Clixml $HOME\Documents\sa.cred
 #endregion
 
+#region Searching and using commands
+
+cd presentations:\
+Return 'Oi Beardy, You may be an MVP but this is a demo, don''t run the whole thing, fool!!'
+
+## Lets look at the commands
+Get-Command -Module dbatools 
+
+## How many Commands?
+(Get-Command -Module dbatools).Count
+
+## How do we find commands?
+Find-DbaCommand -Tag Backup
+Find-DbaCommand -Tag Restore
+Find-DbaCommand -Tag Migration
+Find-DbaCommand -Tag Agent
+Find-DbaCommand -Pattern User 
+Find-DbaCommand -Pattern linked
+
+## How do we use commands?
+
+## ALWAYS ALWAYS use Get-Help
+
+Get-Help Test-DbaLinkedServerConnection -Full
+
+## Here a neat trick
+
+Find-DbaCommand -Pattern Index | Out-GridView -PassThru | Get-Help -Full 
+
+
+
+#endregion
+
 #region Look at Builds
 $builds = @()
 $SQLInstances.ForEach{
