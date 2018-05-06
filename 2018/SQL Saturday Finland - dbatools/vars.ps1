376cfecaf73c1286c40235ce8a8e5fabae83fe07
enterpriseadmin@THEBEARD.LOCAL <enterpriseadmin@THEBEARD.LOCAL>
2018-05-06 11:03:52 +0000

    added variables file

---
+++ 2018/SQL Saturday Finland - dbatools/vars.ps1
@@ -0,0 +1,8 @@
+$SQLInstances = 'sql0', 'sql1'
+$containers = 'bearddockerhost,15789', 'bearddockerhost,15788', 'bearddockerhost,15787', 'bearddockerhost,15786', 'beardlinuxsql'
+$SQL2017Container = 'bearddockerhost,15789'
+$LinuxSQL = 'beardlinuxsql'
+$cred = Import-Clixml $HOME\Documents\sa.cred
+$filenames = (Get-ChildItem C:\SQLBackups\Keep).Name
+$Share = '\\jumpbox.TheBeard.Local\SQLBackups'
+$NetworkShare = '\\bearddockerhost.TheBeard.Local\NetworkSQLBackups'
