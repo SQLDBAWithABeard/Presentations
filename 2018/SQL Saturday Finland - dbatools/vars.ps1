c1603a802c425799a94b1fa962b0897bcfa5c861
enterpriseadmin@THEBEARD.LOCAL <enterpriseadmin@THEBEARD.LOCAL>
2018-05-06 12:49:26 +0000

    dont put linux in the containers

--- 2018/SQL Saturday Finland - dbatools/vars.ps1
+++ 2018/SQL Saturday Finland - dbatools/vars.ps1
@@ -1,5 +1,5 @@
 $SQLInstances = 'sql0', 'sql1'
-$containers = 'bearddockerhost,15789', 'bearddockerhost,15788', 'bearddockerhost,15787', 'bearddockerhost,15786', 'beardlinuxsql'
+$containers = 'bearddockerhost,15789', 'bearddockerhost,15788', 'bearddockerhost,15787', 'bearddockerhost,15786'
 $SQL2017Container = 'bearddockerhost,15789'
 $LinuxSQL = 'beardlinuxsql'
 $cred = Import-Clixml $HOME\Documents\sa.cred
