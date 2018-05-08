fd2a69db9f1c3fbd7ced74e22a587e4c6109124e
enterpriseadmin@THEBEARD.LOCAL <enterpriseadmin@THEBEARD.LOCAL>
2018-05-08 18:00:01 +0000

    measure space

--- 2018/SQL Saturday Finland - dbatools/02 - Demo.ps1
+++ 2018/SQL Saturday Finland - dbatools/02 - Demo.ps1
@@ -111,6 +111,18 @@ Measure-DbaBackupThroughput -SqlInstance $sql0
 ## Check databases on sql1
 Get-DbaDatabase -SqlInstance $sql1 | Format-Table
 
+## Check that we have enough space on the destination (obviously we couldnt do it this way if we SQL0 was broken)
+
+$Databases = (Get-DbaDatabase -SqlInstance $SQL0 -ExcludeAllSystemDb -ExcludeDatabase WideWorldImporters).Name
+$measurement = $Databases.ForEach{
+Measure-DbaDiskSpaceRequirement -Source $SQL0 -Destination $sql1 -Database $PSItem
+}
+
+$measurement.DifferenceSize | Measure-Object -Property Megabyte -Sum
+$measurement.DifferenceSize | Measure-Object -Property GigaByte -Sum
+
+Get-DbaDiskSpace -ComputerName $sql1
+
 ## restore databases from backup folder
 Restore-DbaDatabase -SqlInstance $sql1 -Path $NetworkShare -WithReplace
 
