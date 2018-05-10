47900d2405db90d5427fcfc43271f3facdaf733d
enterpriseadmin@THEBEARD.LOCAL <enterpriseadmin@THEBEARD.LOCAL>
2018-05-10 15:47:49 +0000

    added files

---
+++ 2018/SQL Days - dbachecks/01 - Create.ps1
@@ -0,0 +1,23 @@
+. .\vars.ps1
+
+$verbosePreference = 'Continue'
+#region Create New PSDrive and prompt
+if (-not (Get-PSDrive -Name SQLDays -ErrorAction SilentlyContinue)) {
+    New-PSDrive -Name SQLDays -Root 'C:\Git\Presentations\2018\SQL Days - dbachecks' -PSProvider FileSystem | Out-Null
+    Write-Verbose -Message "Created PSDrive"
+}
+
+function prompt {
+    Write-Host ("Hymyile ja nauti >") -NoNewLine -ForegroundColor Magenta
+    return " "
+}
+
+Write-Verbose -Message "Created prompt"
+
+Set-Location SQLDays:
+
+
+
+
+
+$verbosePreference = 'SilentlyContinue'
