1411440c157f9fa69c48734faf60c47aa5299927
EnterpriseAdmin@TheBeard.Local <EnterpriseAdmin@TheBeard.Local>
2018-05-22 07:44:29 +0000

    exclude FK

--- 2018/SQL Days - dbachecks/02 -Demo.ps1
+++ 2018/SQL Days - dbachecks/02 -Demo.ps1
@@ -25,7 +25,7 @@ Invoke-DbcCheck -SqlInstance $sql0 -Check AutoClose
 
 # its so easy, there is even intellisense
 
-Invoke-DbcCheck -SqlInstance $sql0 -Check 
+Invoke-DbcCheck -SqlInstance $sql0 -Check
 
 ## I am not limited to just one server/instance
 ## Maybe I want to check all my containers for Errors
