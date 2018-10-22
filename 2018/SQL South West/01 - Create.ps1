a5debe675bd782736b08c48d81dd684539823b6f
EnterpriseAdmin@TheBeard.Local <EnterpriseAdmin@TheBeard.Local>
2018-10-22 15:18:56 +0000

    Use the correct path for jumpbox

--- 2018/SQL South West/01 - Create.ps1
+++ 2018/SQL South West/01 - Create.ps1
@@ -3,7 +3,7 @@
 $verbosePreference = 'Continue'
 #region Create New PSDrive and prompt
 if (-not (Get-PSDrive -Name $location -ErrorAction SilentlyContinue)) {
-    New-PSDrive -Name $location -Root 'C:\Users\mrrob\OneDrive\Documents\GitHub\Presentations\2018\SQL Saturday Holland\' -PSProvider FileSystem | Out-Null
+    New-PSDrive -Name $location -Root 'C:\Git\Presentations\2018\SQL South West' -PSProvider FileSystem | Out-Null
     Write-Verbose -Message "Created PSDrive"
 }
 
