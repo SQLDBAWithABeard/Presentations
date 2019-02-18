6135ba2a9eb9096b5c9fd2374ee2bf3b08a268eb
Rob Sewell <mrrobsewell@outlook.com>
2019-02-18 09:05:53 +0000

    more funky prompt

--- 2019/SQLBits/vars.ps1
+++ 2019/SQLBits/vars.ps1
@@ -1,3 +1,4 @@
+[datetime]$EndDate = '2019-02-18 08:22'
 if ($ENV:COMPUTERNAME -eq 'JumpBox') {
     $cred = Import-Clixml $HOME\Documents\sa.cred
     $SQLInstances = 'sql0', 'sql1'
@@ -28,3 +29,61 @@ if (-not ($PSDefaultParameterValues.'*-Dba*:SqlCredential')) {
     }
 }
 $location = 'Manchester'
+
+function Prompt { 
+    $Date = Get-Date
+    $Mins = ($EndDate - $Date).TotalMinutes
+
+    switch ($Mins) {
+        {$_ -ge 30} { 
+            $ToGo = [Math]::Round($mins,1)
+            $Time = $Date.ToShortTimeString()
+            Write-Host "$Time $ToGo Mins" -ForegroundColor DarkGreen -NoNewline
+         }
+         {$_ -lt 30 -and $_ -gt 10}{
+            $ToGo = [Math]::Round($mins,1)
+            $Time = $Date.ToShortTimeString()
+            Write-Host "$Time " -ForegroundColor DarkGreen -NoNewline
+            Write-Host "$ToGo Mins" -ForegroundColor Yellow -NoNewline
+         }
+         {$_ -le 10}{
+            $ToGo = [Math]::Round($mins,1)
+            $Time = $Date.ToShortTimeString()
+            Write-Host "$Time " -ForegroundColor DarkGreen -NoNewline
+            Write-Host "$ToGo Mins" -ForegroundColor Red -NoNewline
+         }
+        Default {}
+    }
+    
+    try {
+        $history = Get-History -ErrorAction Ignore -Count 1
+        if ($history) {
+            $ts = New-TimeSpan $history.StartExecutionTime  $history.EndExecutionTime
+            switch ($ts) {
+                {$_.totalminutes -gt 1 -and $_.totalminutes -lt 30  } {
+                    Write-Host " [" -ForegroundColor Red -NoNewline
+                    [decimal]$d = $_.TotalMinutes
+                    '{0:f3}m' -f ($d) | Write-Host  -ForegroundColor Red -NoNewline
+                    Write-Host "]" -ForegroundColor Red -NoNewline
+                }
+                {$_.totalminutes -le 1 -and $_.TotalSeconds -gt 1} {
+                    Write-Host " [" -ForegroundColor Yellow -NoNewline
+                    [decimal]$d = $_.TotalSeconds
+                    '{0:f3}s' -f ($d) | Write-Host  -ForegroundColor Yellow -NoNewline
+                    Write-Host "]" -ForegroundColor Yellow -NoNewline
+                }
+                {$_.TotalSeconds -le 1} {
+                    [decimal]$d = $_.TotalMilliseconds
+                    Write-Host " [" -ForegroundColor Green -NoNewline
+                    '{0:f3}ms' -f ($d) | Write-Host  -ForegroundColor Green -NoNewline
+                    Write-Host "]" -ForegroundColor Green -NoNewline
+                }
+                Default {
+                    $_.Milliseconds | Write-Host  -ForegroundColor Gray -NoNewline
+                }
+            }
+        }
+    }
+    catch { }
+    "> "
+  }
