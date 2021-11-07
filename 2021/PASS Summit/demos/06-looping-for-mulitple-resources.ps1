29cd05e9e5e2bdc75c908c729682aa431d7923ca
SQLDbaWithABeard <mrrobsewell@outlook.com>
2021-11-07 08:51:15 +0000

    open file

--- 2021/PASS Summit/demos/06-looping-for-mulitple-resources.ps1
+++ 2021/PASS Summit/demos/06-looping-for-mulitple-resources.ps1
@@ -105,6 +105,8 @@ Get-DbaDatabase -SqlInstance $azureSql | Select SqlInstance , Name, IsAccessible
 
 #region deploy with an array of names
 
+code '..\Bicep\myCustomResources\05sqlserveranddatabaseswithlooprange.bicep'
+
 $deploymentConfig = @{
     ResourceGroupName          = 'PassBeard'
     TemplateFile               = '..\Bicep\myCustomResources\05sqlserveranddatabaseswithlooparray.bicep'
