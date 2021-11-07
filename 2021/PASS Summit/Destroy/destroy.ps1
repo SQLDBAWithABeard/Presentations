5f01042035390c502d717c343d10356ac4ddf74e
SQLDbaWithABeard <mrrobsewell@outlook.com>
2021-11-07 09:19:13 +0000

    do it in parallel

--- 2021/PASS Summit/Destroy/destroy.ps1
+++ 2021/PASS Summit/Destroy/destroy.ps1
@@ -1,4 +1,4 @@
-$rgnames = 'PassBeard', 'PassBeards', 'PassBeard-Admin', 'beardednetwork-rg', 'beardynetwork-dev-rg', 'beardynetwork-uat-rg', 'beardynetwork-rg','demo-rg','beard-admin-rg'
+$_names = 'PassBeard', 'PassBeards', 'PassBeard-Admin', 'beardednetwork-rg', 'beardynetwork-dev-rg', 'beardynetwork-uat-rg', 'beardynetwork-rg','demo-rg','beard-admin-rg'
 $films = 'Iron-Man', 'The-Incredible-Hulk', 'Iron-Man-2', 'Thor', 'Captain-America-The-First-Avenger', 'Marvels-The-Avengers', 'Iron-Man-3', 'Thor-The-Dark-World', 'Captain-America-The-Winter-Soldier', 'Guardians-of-the-Galaxy',	'Avengers-Age-of-Ultron', 'Ant-Man',	'Captain-America-Civil-War', 'Doctor-Strange',	'Guardians-of-the-Galaxy-2', 'Spider-Man-Homecoming', 'Thor-Ragnarok',	'Black-Panther',	'Avengers-Infinity-War', 'Ant-Man-and-the-Wasp', 'Captain-Marvel',	'Avengers-Endgame', 'Spider-Man-Far-From-Home', 'Black-Widow'
 $filmrgs = foreach ($env in 'dev', 'uat', 'prod') {
     foreach ($film in $films) {
@@ -29,26 +29,26 @@ $filmrgs | ForEach-Object -Parallel  {
     }
 } -ThrottleLimit 25
 
-foreach ($rg in $rgnames) {
-    $r = Get-AzResourceGroup -Name $rg -ErrorAction SilentlyContinue
+$_names | ForEach-Object -Parallel  {
+    $r = Get-AzResourceGroup -Name $_ -ErrorAction SilentlyContinue
     if ($r) {
-        $message = "We have a resource group called {0}" -f $rg
+        $message = "We have a resource group called {0}" -f $_
         Write-PSFMessage $message -Level Output
         try {
-            $message = "###   Removing resource group {0}" -f $rg
+            $message = "###   Removing resource group {0}" -f $_
             Write-PSFMessage $message -Level Output
-            Remove-AzResourceGroup -Name $rg -Confirm:$false -Force | Out-Null
-            $message = "###   Resource group {0} Removed" -f $rg
+            Remove-AzResourceGroup -Name $_ -Confirm:$false -Force | Out-Null
+            $message = "###   Resource group {0} Removed" -f $_
             Write-PSFMessage $message -Level Output
         }
         catch {
-            $message = "###   FAILED - REMOVING -Resource group {0}" -f $rg
+            $message = "###   FAILED - REMOVING -Resource group {0}" -f $_
             Write-PSFMessage $message -Level Significant
         }
     }
     else {
-        $message = "There is no resource group called {0}" -f $rg
+        $message = "There is no resource group called {0}" -f $_
         Write-PSFMessage $message -Level Output
     }
-}
+} -ThrottleLimit 25
 
