458d3b8b2fa04d4dc6baa5cfbefdd86444f6b035
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-05-30 13:31:59 +0100

    so that we set the compat level

--- 2022/DataGrillen/datagrillen1/scripts/restore-db.sql
+++ 2022/DataGrillen/datagrillen1/scripts/restore-db.sql
@@ -2,3 +2,4 @@ USE [master]
 RESTORE DATABASE [AdventureWorks] FROM  DISK = N'/tmp/AdventureWorks2017.bak'  WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2019.mdf', MOVE 'AdventureWorks2017_Log' TO '/var/opt/mssql/data/AdventureWorks2019_Log.ldf'
 GO
 ALTER DATABASE [AdventureWorks] SET COMPATIBILITY_LEVEL = 150
+ALTER DATABASE [AdventureWorks] SET QUERY_STORE = ON;
